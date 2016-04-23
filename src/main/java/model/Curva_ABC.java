f90e938cd7bfbdfb94de67ae6f292c0757913913
Gabriel Santos <gabrieelsantosn@gmail.com>
2016-04-23 20:47:29 -0300

    Teste de conexão com o banco de dados.

--- src/main/java/model/Curva_ABC.java
+++ src/main/java/model/Curva_ABC.java
@@ -1,5 +1,14 @@
 package model;
 
-public class Curva_ABC {
+import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="curva_abc")
+public class Curva_ABC {
+	private Long id_curva;
+	private List<Material> materiais;
+	private char classificao;
 }
