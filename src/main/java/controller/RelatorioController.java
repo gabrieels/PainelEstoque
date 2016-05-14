4a380de8462a4d1e07f572fc060c7a6d35064e1f
Danilo Gois dos Anjos <dangois12@gmail.com>
2016-05-14 17:02:01 -0300

    Alteração na classe RelatorioController

--- src/main/java/controller/RelatorioController.java
+++ src/main/java/controller/RelatorioController.java
@@ -1,11 +1,10 @@
 package controller;
 
-
- 
 import javax.annotation.PostConstruct;
 import java.io.Serializable;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import javax.faces.bean.ManagedBean;
 import org.primefaces.model.chart.Axis;
@@ -14,7 +13,10 @@ import org.primefaces.model.chart.CategoryAxis;
 import org.primefaces.model.chart.LineChartModel;
 import org.primefaces.model.chart.ChartSeries;
 import org.primefaces.model.chart.LineChartSeries;
- 
+
+import persistencia.entidade.Relatorio;
+import persistencia.jdbc.RelatorioDAO;
+
 @ManagedBean
 public class RelatorioController implements Serializable {
 	/**
@@ -22,88 +24,51 @@ public class RelatorioController implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Date date;
-    private LineChartModel lineModel;
-     
-    @PostConstruct
-    public void init() {
-        createLineModels();
-    }
- 
-    public LineChartModel getLineModel() {
-        return lineModel;
-    }
- 
-    
-    
-     
-    private void createLineModels() {
-        lineModel = initLinearModel();
-        Axis yAxis = lineModel.getAxis(AxisType.Y);
-        yAxis.setMin(0);
-        yAxis.setMax(10);
-         
-        lineModel.setTitle(obterAno());
-        lineModel.setLegendPosition("e");
-        lineModel.setShowPointLabels(true);
-        lineModel.getAxes().put(AxisType.X, new CategoryAxis(""));
-        yAxis = lineModel.getAxis(AxisType.Y);
-        yAxis.setLabel("Consumo");
-        yAxis.setMin(0);
-        yAxis.setMax(200);
-    }
-     
-    private LineChartModel initLinearModel() {
-        LineChartModel model = new LineChartModel();
- 
-        LineChartSeries series = new LineChartSeries();
-        series.setLabel("Linha de consumo");
-        
-                        
-        series.set("Janeiro", 2);
-        series.set("Fevereiro", 3);
-        series.set("Março", 10);
-        series.set("Abril", 50);
-        series.set("Maio", 10); 
-        series.set("Junho", 170);
-        series.set("Julho", 80);
-        series.set("Agosto", 35);
-        series.set("Setembro", 35);
-        series.set("Outrubro", 35);
-        series.set("Novembro", 35);
-        series.set("Dezembro", 35);
-        model.addSeries(series);
-        
-         
-        return model;
-    }
-     
-    private LineChartModel initCategoryModel() {
-        LineChartModel model = new LineChartModel();
- 
-        ChartSeries mes = new ChartSeries();
-        String ano = obterAno();
-        mes.setLabel("Linha de Consumo " + ano);
-        
-		mes.set("Janeiro", 50);
-		mes.set("Feveiro", 170);
-		mes.set("Marco", 80);
-		mes.set("Abril", 35);
-		mes.set("Maio", 50);
-		mes.set("Junho", 170);
-		mes.set("Julho", 80);
-		mes.set("Agosto", 35);
-		mes.set("Setembro", 35);
-		mes.set("Outrubro", 35);
-		mes.set("Novembro", 35);
-		mes.set("Dezembro", 35);
-		
-		model.addSeries(mes);
 
+	private List<Relatorio> relatorios = null;
+	private ChartSeries relatorioData;
+	private LineChartModel lineModel;
+
+	public List<Relatorio> gerarRelatorio() {
+		RelatorioDAO relatorioDAO = new RelatorioDAO();
+		relatorios = relatorioDAO.gerarRelatorio();
+		createLineModels();
+		return relatorios;
+	}
+
+	public LineChartModel getLineModel() {
+		return lineModel;
+	}
+
+	@PostConstruct
+	private LineChartModel initLineModel() {
+		LineChartModel model = new LineChartModel();
+
+		relatorioData = new ChartSeries();
+		relatorioData.setLabel("Consumo");
+		for (Relatorio relatorio : relatorios) {
+			relatorioData.set(relatorio.getMes_saida(), relatorio.getValor_consumido());
+		}
+
+		model.addSeries(relatorioData);
 		return model;
-	
-    }
- 
+	}
 
+	private void createLineModels() {
+		lineModel = initLineModel();
+
+		lineModel.setTitle("Relatorio 2016");
+		lineModel.setLegendPosition("ne");
+
+		Axis xAxis = lineModel.getAxis(AxisType.X);
+		xAxis.setLabel("MÃªs");
+
+		Axis yAxis = lineModel.getAxis(AxisType.Y);
+		yAxis.setLabel("Consumo");
+		yAxis.setMin(0);
+		yAxis.setMax(1000);
+
+	}
 
 	// retorna o ano atual do sistema
 	public String obterAno() {
